b50f4262bbd9f54bdb96f8b97a9d091736773a17
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-10-18 21:29:31 -0400

    nvm

--- src/components/projects/mongo.js
+++ src/components/projects/mongo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core';
+import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
