ac97071b1ff3932aea0252730558745db0a76a56
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-10-18 21:16:55 -0400

    Updates

---
+++ precache-manifest.58269274d11664caa87b8907543f9b60.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e2761f216fbca655a15e5c6a8e563b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "46977de178557e2e62fc",
+    "url": "/react-portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "8010626b7183b25b71ca",
+    "url": "/react-portfolio/static/css/main.f3931116.chunk.css"
+  },
+  {
+    "revision": "46977de178557e2e62fc",
+    "url": "/react-portfolio/static/js/2.3c60de79.chunk.js"
+  },
+  {
+    "revision": "633ce508062cdafc4f8ece51f4d45ac9",
+    "url": "/react-portfolio/static/js/2.3c60de79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8010626b7183b25b71ca",
+    "url": "/react-portfolio/static/js/main.c05b5d8f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "00d85a52be9ba461ebfa929a4850a6fa",
+    "url": "/react-portfolio/static/media/Demo.00d85a52.gif"
+  },
+  {
+    "revision": "b3e1b44ac95475999059eadbb46efbd0",
+    "url": "/react-portfolio/static/media/IMG_3601.b3e1b44a.jpg"
+  },
+  {
+    "revision": "3d50c84101965d6c69afc5ba5de1f914",
+    "url": "/react-portfolio/static/media/budgetT.3d50c841.png"
+  },
+  {
+    "revision": "369c1751c0cae3125cfbc86e37cb6aca",
+    "url": "/react-portfolio/static/media/dashboard.369c1751.png"
+  },
+  {
+    "revision": "670330e6163ad502a3f4615daa6e31aa",
+    "url": "/react-portfolio/static/media/download.670330e6.png"
+  },
+  {
+    "revision": "dfbd5a272ce500e33780980dd5581868",
+    "url": "/react-portfolio/static/media/eTracker.dfbd5a27.gif"
+  },
+  {
+    "revision": "834bb637c0cf4c986c6b2ca7619ec528",
+    "url": "/react-portfolio/static/media/img2.834bb637.png"
+  },
+  {
+    "revision": "69f129c6dfb96cee2a286aade52170ff",
+    "url": "/react-portfolio/static/media/img3.69f129c6.png"
+  },
+  {
+    "revision": "885db37ce0c7f2ce64541d70307a0c87",
+    "url": "/react-portfolio/static/media/proj1.885db37c.png"
+  }
+]);
